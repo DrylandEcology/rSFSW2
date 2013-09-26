57f9b1cb49ccfa9c8175f1b886320412b24abfd8
dschlaep <danirsch@gmail.com>
2013-09-26 10:02:52 -0600

    verbosity for copying current conditions into database

--- 2_SoilWatSimulationFramework_Part3of3_Code_v50.R
+++ 2_SoilWatSimulationFramework_Part3of3_Code_v50.R
@@ -5368,7 +5368,9 @@ if(makeOutputDB && any(actions=="concatenate")) {
 		}
 		
 		if(!be.quiet) print(paste("Database complete in :",  round(difftime(Sys.time(), t1, units="secs"), 2), "s"))
+		
 		if(copyCurrentConditions) {
+			if(!be.quiet) print(paste("Database is copied and subset to ambient condition: start at ",  Sys.time()))
 			#Get sql for tables and index
 			resSQL<-dbSendQuery(con, "SELECT sql FROM sqlite_master WHERE type='table' ORDER BY name;")
 			sqlTables <- fetch(resSQL,n=-1)
