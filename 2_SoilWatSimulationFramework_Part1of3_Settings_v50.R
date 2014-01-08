6a7bd9bbfb8c1d992e23c2ab3dbdc25722afd05c
dschlaep <danirsch@gmail.com>
2014-01-08 10:14:45 -0700

    deleted some more old variables

--- 2_SoilWatSimulationFramework_Part1of3_Settings_v50.R
+++ 2_SoilWatSimulationFramework_Part1of3_Settings_v50.R
@@ -234,11 +234,6 @@ copyCurrentConditionsFromTempSQL <- TRUE
 copyCurrentConditionsFromDatabase <- FALSE #Creates a copy of the main database containing the scenario==climate.ambient subset
 ensembleCollectSize <- 500 #This value is the chunk size for reads of 'runID' from the database, i.e., chunk size = ensembleCollectSize * scenario_No. Yellowstone 500 seems to work. Balance between available memory, cores, read/write times, etc..
 
-#Type of concatenation (if not DB used)
-concatenation.inMemory <- FALSE #concatenation.inMemory: all temp output is loaded into a giant data.frame, then written to final file; if !concatenation.inMemory, temp output is read and immediately appended to final file
-#deletes all temporary files in the dir.out.temp folder after successful concatenation if TRUE
-deleteTemporaryAggregationFiles <- TRUE
-
 #------Define type of simulations and source of input data
 #indicate whether to obtain external information (1) or not (0), don't delete any labels; GIS extractions not supported on JANUS
 do.ExternalInformation <- c(
