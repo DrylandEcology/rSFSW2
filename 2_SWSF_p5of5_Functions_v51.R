c78c6a233fbdc4d5ab420114f4f1fe67f30903b2
dschlaep <daniel.schlaepfer@unibas.ch>
2016-08-23 09:23:18 +0200

    Moved some functions from p3 to p5
    TODO
        - most functions are defined inside conditions with different code
        - many global variables used
    
    
    Former-commit-id: 76d95f1b62bc0a03d3f9dee4ded992f1beb34c77 [formerly 4f3b586d4767215fc833a61f12e0c5d8130a68e4]
    Former-commit-id: c0928970d76bcd98d4e03e0a41298d8175a95845

--- 2_SWSF_p5of5_Functions_v51.R
+++ 2_SWSF_p5of5_Functions_v51.R
@@ -10,3 +10,19 @@ getSiteIds <- function(con, folderNames) {
   wf_ids[match(folderNames, wf_ids[, "folder"], nomatch = NA), "id"]
 }
 
+has_nodata <- compiler::cmpfun(function(data, tag = NULL, MARGIN = 1) {
+  if (is.null(tag)) {
+    apply(data, MARGIN, function(x) all(is.na(x)))
+  } else {
+    apply(data[, grepl(tag, colnames(data)), drop = FALSE], MARGIN, function(x) all(is.na(x)))
+  }
+})
+
+has_incompletedata <- compiler::cmpfun(function(data, tag = NULL, MARGIN = 1) {
+  if (is.null(tag)) {
+    apply(data, MARGIN, anyNA)
+  } else {
+    apply(data[, grepl(tag, colnames(data)), drop = FALSE], MARGIN, anyNA)
+  }
+})
+
