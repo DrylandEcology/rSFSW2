ae5d67e4d11d2e6cfba610bc62289cea33a1221c
dschlaep <daniel.schlaepfer@unibas.ch>
2016-10-06 10:59:11 +0200

    Rcpp version of function 'get_KilledBySoilLayers'
    - setting SWSF up to handle Rcpp: DESCRIPTION, NAMESPACE, src/,
    sourcing script ‘SWSF_cpp_functions.R’ on each node
    - script ‘SWSF_cpp_functions.R’ generates the function
    ‘get_KilledBySoilLayers’: from c++ code
    ‘src/GISSM_get_KilledBySoilLayers.cpp’ if Rcpp package available —
    otherwise using the old R version as fallback
    
    Profiling the function do_OneSite suggested that
    'get_KilledBySoilLayers' was the biggest bottleneck time- and
    memory-wise: The Rcpp version of the function is about 165x faster than
    the version previous to commit 748161302b05d408b8873a91dc21ab4564de2ea7 [formerly ba61e7290370373b528248894d8ac339286d50ac] [formerly d3b5502149fe4ff38b9af551d646f57d20c7f47c [formerly f32ab03b468511341c9fe7bffae929f2ad775a65]]
    'Small speed improvements' and about 70x faster than the R version. The
    Rcpp version also reduced the memory footprint by a factor of 200.
    
    Tests:
    Both versions (Rcpp and R) pass the unit tests in
    ‘test_GISSM_get_KilledBySoilLayers.R’
    
    Former-commit-id: 91f8b0e0a4a7bd802be740d3c44f8f7a5a090a23 [formerly 86e349dbce23805d7f6c1ad060551940228d4a30]
    Former-commit-id: cebd3258b16a50bbe88fe4881936c95a9e7df4c5

---
+++ R/SWSF_cpp_functions.R
@@ -0,0 +1,44 @@
+#------ Remove when this becomes a R package
+
+
+########################
+#------ GISSM functions
+# Schlaepfer, D.R., Lauenroth, W.K. & Bradford, J.B. (2014). Modeling regeneration responses of big sagebrush (Artemisia tridentata) to abiotic conditions. Ecol Model, 286, 66-77.
+
+
+#' Determine if all conditions across rooted soil layers are deadly
+#'
+#' Function that checks whether all relevant (those with roots) soil layers are under conditions of mortality (kill.conditions) for each day of a given year
+#'
+#'  \code{relevantLayers} takes either \code{NA} if no soil layers should be considered
+#'  (e.g., because not yet germinated), or an integer number between 1 and the number of
+#'  simulated soil layers. The number indicates the depth to which a seedling has grown
+#'  roots and over which layers \code{kill.conditions} will be evaluated.
+#'
+#' @setion: Note: The Rcpp version of the function is about 165x faster than the version
+#'  previous to commit 6344857a9cdb08acf68fa031c43cf4a596613aad 'Small speed improvements'
+#'  and about 70x faster than the R version. The Rcpp version also reduced the memory
+#'  footprint by a factor of 200.
+#'
+#' @param relevantLayers An integer vector, usually of length 365 or 366 (days).
+#' @param kill.conditions A m x p logical matrix with \code{m >= length(relevantLayers)}
+#'  and p represents the number of simulated soil layers, i.e.,
+#'  \code{p >= max(relevantLayers, na.rm = TRUE)}.
+#'
+#' @return A logical vector of the length of \code{relevantLayers} with values containing
+#'  \code{NA} for days when conditions were not evaluated, \code{TRUE} if all
+#'  relevant soil layers (columns) of \code{kill.conditions} were \code{TRUE}, and with
+#'  \code{FALSE} otherwise
+if (requireNamespace("Rcpp")) {
+  Rcpp::sourceCpp(file.path(dir.code, "..", "src", "GISSM_get_KilledBySoilLayers.cpp"))
+
+} else {
+  get_KilledBySoilLayers <- compiler::cmpfun(function(relevantLayers, kill.conditions) {
+    vapply(seq_along(relevantLayers), function(k)
+        all(as.logical(kill.conditions[k, if (is.finite(relevantLayers[k])) seq_len(relevantLayers[k]) else NA])),
+      FUN.VALUE = NA)
+  })
+}
+
+#------ End of GISSM functions
+########################
