f783c9b5793f43e3c1c56f366f9658daeb9e352c
charlieduso <dusocharles@hotmail.com>
2016-12-05 14:44:41 -0700

    Modified Livneh Extraction script with additional comments and added function call to actual extraction

--- R/Livneh-Extraction.R
+++ R/Livneh-Extraction.R
@@ -1,10 +1,42 @@
-#' @title Extract Weather Data from a Livneh Database
+########################################################
+# Livneh Gridded, Daily Weather Data Extraction
+#
+# Author - Charles Duso
+# Date   - December 5th, 2016
+########################################################
+
+########################################################
+# Helper function to determine if a year is a leap year
+########################################################
+is_leapyear <- function(year){
+  return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
+}
+
+###################################################
+# Helper function to convert ncdf files to rasters
+###################################################
+convert_to_brick <- function(fileName, type) {
+  r <- raster::brick(fileName, varname=type)
+}
+
+###################################################################
+# Helper function to convert coordinates to the correct resolution
+###################################################################
+conv_res <- function(x) {
+  round((28.15625+round((x-28.15625)/0.0625,0)*0.0625), digits=5)
+}
+
+
+#' @title Extract Gridded Weather Data from a Livneh Database
 #'
-#' @description Extracts daily weather data, including precipitation,
-#' maximum temperature and minimum temperature
+#' @description Extracts daily gridded weather data, including precipitation,
+#'              maximum temperature and minimum temperature from the Livneh 
+#'              database: a 1/16 degree gridded weather database that contains 
+#'              data for the years 1915 - 2011. 
+#' @references  \href{http://www.esrl.noaa.gov/psd/data/gridded/data.livneh.html}{Livneh Weather Website}
 #'
 #' @param    dir_data        directory containing Livneh data
-#' @param    dir_db          the database directory
+#' @param    dir_temp          the database directory
 #' @param    site_ids        the sites to gather weather data for
 #' @param    coords          the coordinates for each site in WGS84 format
 #' @param    start_year      the start year in the sequence of data to gather
@@ -22,15 +54,13 @@
 #'
 #' @author   Charles Duso    <cd622@@nau.edu>
 #' @export
-extract_daily_weather_from_livneh_2013_north_america <-
-                                          function(dir_data, dir_db,
-                                                   site_ids, coords,
-                                                   start_year, end_year,
-                                                   f_check = TRUE,
-                                                   backup = TRUE,
-                                                   comp_type = "gzip",
-                                                   run_parallel = FALSE,
-                                                   num_cores = 0) {
+extract_daily_weather_from_livneh <- function(dir_data, dir_temp, site_ids,
+                                              coords, start_year, end_year,
+                                              f_check = TRUE, backup = TRUE,
+                                              comp_type = "gzip",
+                                              run_parallel = FALSE,
+                                              num_cores = 0,
+                                              be_quiet  = TRUE) {
 
     ########################################
     # Ensure necessary libraries are loaded
@@ -47,7 +77,9 @@ extract_daily_weather_from_livneh_2013_north_america <-
 
     # Start timer for timing the extraction process
     t_elapsed        <- proc.time()
-    print("Preparing to extract weather data from Livneh database.")
+    if (!be_quiet) {
+      print("Preparing to extract weather data from Livneh database.")
+    }
 
     # Go to the directory for the weather database for extraction
     setwd(dir_data)
@@ -55,7 +87,9 @@ extract_daily_weather_from_livneh_2013_north_america <-
 
     # Verify the data's integrity before executing
     if (f_check) {
-      print("Verifying data integrity.")
+      if (!be_quiet) {
+        print("Verifying data integrity.")
+      }
       db_months        <- unique(substr(db_files, 44, 45))
       db_years         <- unique(substr(db_files, 40, 43))
       f_count         <- 1
@@ -63,19 +97,25 @@ extract_daily_weather_from_livneh_2013_north_america <-
         for (j in 1:length(db_months)) {
           fName      <- paste(db_years[i], db_months[j], sep="")
           if (!grepl(pattern = fName, db_files[f_count])) {
-            print(paste("ERROR: Monthly data file is missing for year-month: ",
-                        db_years[i], "-", db_months[j], sep=""))
+            if (!be_quiet) {
+              print(paste("ERROR: Monthly data file is missing for year-month: ",
+                          db_years[i], "-", db_months[j], sep=""))
+            }
             stop()
 
           }
           f_count = f_count + 1
         }
       }
-      print("Data integrity has been verified; no errors have been detected.")
+      if (!be_quiet) {
+        print("Data integrity has been verified; no errors have been detected.")
+      }
     }
 
     # Refine coordinates to resolution suitable for Livneh
-    print("Refining coordinates to match database resolution.")
+    if (!be_quiet) {
+      print("Refining coordinates to match database resolution.")
+    }
     colnames(coords)      <- NULL
     coords                <- na.omit(coords)
     xy_wgs84              <- matrix(unlist(coords), ncol = 2)
@@ -93,7 +133,9 @@ extract_daily_weather_from_livneh_2013_north_america <-
     site_length           <-  length(site_ids)
     data_sw               <-  array(dim = c(site_length, 366, 3, len_years))
 
-    print("Extracting data for supplied sequence of years.")
+    if (!be_quiet) {
+      print("Extracting data for supplied sequence of years.")
+    }
 
     # Prepare parallel extraction if set to TRUE
     if (run_parallel) {
@@ -111,10 +153,12 @@ extract_daily_weather_from_livneh_2013_north_america <-
       # Get data files for respective year
       files <- db_files[j:(j + 11)]
 
-      print(paste("Extracting data for year ", seq_years[i], sep=""))
-      print("Files: ")
-      print(files)
-      print("================================")
+      if (!be_quiet) {
+        print(paste("Extracting data for year ", seq_years[i], sep=""))
+        print("Files: ")
+        print(files)
+        print("================================")
+      }
 
       # Extract Weather Data as Raster Stacks
       l_brick    <- lapply(files, convert_to_brick, type="Prec")
@@ -159,18 +203,24 @@ extract_daily_weather_from_livneh_2013_north_america <-
     }
 
     # Connect to the weather database
-    setwd(dir_db)
-    Rsoilwat31:::dbW_setConnection(file.path(dir_db, "dbWeatherData.sqlite"))
+    setwd(dir_temp)
 
     # Backup RData in the event of an error with insertion
     if (backup) {
-      print("Backing up data object.")
+      if (!be_quiet) {
+        print("Backing up data object.")
+      }
       save(data_sw, file="weathData.RData")
-      print("Data object has been backed-up.")
+      if (!be_quiet) {
+        print("Data object has been backed-up.")
+      }
     }
 
     # Format data and add it to the weather database
-    print("Inserting data into weather database.")
+    if (!be_quiet) {
+      print("Inserting data into weather database.")
+    }
+
     for (i in 1:site_length) {
       weather_data <- list()
       for (k in 1:len_years) {
@@ -200,17 +250,34 @@ extract_daily_weather_from_livneh_2013_north_america <-
     # Clean-up environment
     #######################
 
-    print("Weather data has been successfully inserted.")
+    if (!be_quiet) {
+      print("Weather data has been successfully inserted.")
+    }
 
     # Remove files & clean garbage to free-up RAM for executions that
     # don't just involve database creation
-    print("Cleaning up garbage.")
+    if (!be_quiet) {
+      print("Cleaning up garbage.")
+    }
     gc()
     rm(weather_data, out, data_sw, data_blob, l_stack, l_brick, tmax, tmin,
        prec)
 
     # End timer and notify user that extraction has finished
-    print("Data has been inserted.")
-    print(proc.time() - t_elapsed)
+    if (!be_quiet) {
+      print("Data has been inserted.")
+      print(proc.time() - t_elapsed)
+    }
     invisible(0)
 }
+
+
+#######################
+# Extract weather data
+#######################
+extract_daily_weather_from_livneh(dir_data = dir.ex.Livneh, dir_temp = dir.out.temp, 
+                                  site_ids = SWRunInformation$site_id[ids_Livneh], 
+                                  coords = SWRunInformation[ids_Livneh, c("X_WGS84", "Y_WGS84"), drop = FALSE], 
+                                  start_year = simstartyr, end_year = endyr, 
+                                  comp_type = dbW_compression_type, 
+                                  run_parallel = parallel_runs, num_cores = num_cores, be_quiet = be.quiet)
