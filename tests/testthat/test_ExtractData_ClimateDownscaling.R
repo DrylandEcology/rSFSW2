de7940b99539b2c55143a0056f30b780bcb9810e
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2017-07-27 15:46:25 -0400

    Fix 'test_ExtractData_ClimateDownscaling.R'
    Use the correct 'testthat' function...

--- tests/testthat/test_ExtractData_ClimateDownscaling.R
+++ tests/testthat/test_ExtractData_ClimateDownscaling.R
@@ -7,7 +7,7 @@ temp_K <- 273.15 + temp_C
 
 
 #---TESTS
-expect_that("Unit conversion", {
+test_that("Unit conversion", {
   expect_equal(convert_temperature(temp_C, "C"), temp_C)
   expect_equal(convert_temperature(temp_F, "F"), temp_C, tolerance = 1e-6)
   expect_equal(convert_temperature(temp_K, "K"), temp_C)
