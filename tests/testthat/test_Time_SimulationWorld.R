e358860373b4d1373a5808194515294a9b370251
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2017-08-02 18:40:40 -0400

    Add unit tests for 'simTiming_ForEachUsedTimeUnit'
    This demonstrates issue #147:
    
    > test_file("tests/testthat/test_Time_SimulationWorld.R")
    > Time in the simulation world: .......................1..........
    >
    > Failed
    ------------------------------------------------------------------------
    --
    > 1. Failure: Obtain time information (@test_Time_SimulationWorld.R#62)
    -----------
    > length(sim_time2_South[[k]]) not equal to `n_days`.
    > 1/1 mismatches
    > [1] 11322 - 11323 == -1
    > For k = 'year_ForEachUsedDay_NSadj'
    >
    > DONE
    ========================================================================
    ====

--- tests/testthat/test_Time_SimulationWorld.R
+++ tests/testthat/test_Time_SimulationWorld.R
@@ -13,6 +13,10 @@ input_sim_time <- list(
   )
 )
 
+expected_sim_time_elements <- c("simstartyr", "startyr", "endyr", "DScur_startyr", "DScur_endyr", 
+"future_yrs", "spinup_N", "future_N", "useyrs", "no.useyr", "no.usemo", 
+"no.usedy", "index.useyr", "index.usemo", "index.usedy")
+
 input_sim_time2 <- input_sim_time
 input_sim_time2[["future_yrs"]] <- list(
     c(d <- 40, startyr + d, endyr + d),
@@ -26,25 +30,42 @@ test_that("Obtain time information", {
   expect_equal(getStartYear(0), 1L)
   expect_equal(getStartYear(0, 10), 10L)
 
+
   # Leap years
   expect_true(isLeapYear(2000))
   expect_true(isLeapYear(2016))
   expect_false(isLeapYear(2100))
   expect_false(isLeapYear(2003))
 
+
   # Sequence of month numbers for each day in the period
   expect_equal(seq_month_ofeach_day(list(1980, 1, 1), list(2010, 12, 31), tz = "UTC"),
     as.POSIXlt(seq(from = ISOdate(1980, 1, 1, tz = "UTC"),
     to = ISOdate(2010, 12, 31, tz = "UTC"), by = "1 day"))$mon + 1)
 
+
   # Setup simulation time
-  expect_silent(sim_time <- setup_simulation_time(input_sim_time))
+  expect_silent(sim_time <- setup_simulation_time(input_sim_time, 
+    add_st2 = TRUE, adjust_NS = TRUE))
   expect_error(setup_simulation_time(input_sim_time2),
     regexp = "incorrect format of 'future_yrs'")
   expect_equal(sim_time[["useyrs"]], sim_time[["startyr"]]:sim_time[["endyr"]])
+  expect_true(all(expected_sim_time_elements %in% names(sim_time)))
 
   # Simulation time aggregation lists
   expect_silent(sim_time2_North <- simTiming_ForEachUsedTimeUnit(sim_time, latitude = 90))
   expect_silent(sim_time2_South <- simTiming_ForEachUsedTimeUnit(sim_time, latitude = -90))
+  n_days <- sim_time[["no.usedy"]]
+  for (k in grep("ForEachUsedDay", names(sim_time2_North), value = TRUE)) {
+    info <- paste("For k =", shQuote(k))
+    expect_equal(length(sim_time2_North[[k]]), n_days, info = info)
+    expect_equal(length(sim_time2_South[[k]]), n_days, info = info)
+  }
+  n_months <- sim_time[["no.usemo"]]
+  for (k in grep("ForEachUsedMonth", names(sim_time2_North), value = TRUE)) {
+    info <- paste("For k =", shQuote(k))
+    expect_equal(length(sim_time2_North[[k]]), n_months, info = info)
+    expect_equal(length(sim_time2_South[[k]]), n_months, info = info)
+  }
 })
 
