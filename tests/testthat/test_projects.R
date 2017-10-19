7ce6e0d325cdfa0b9e3ad8df3e027e077960268b
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2017-10-19 14:51:30 -0400

    Updated unit test 'test_projects'
    - updated output reference database to rSFSW2 version 2.3.15
    - provide more verbose output if one of the unit tests fails

--- tests/testthat/test_projects.R
+++ tests/testthat/test_projects.R
@@ -13,12 +13,17 @@ test_that("Test projects", {
         dir_prj_tests = ".", dir_ref = file.path("..", "test_data", "0_ReferenceOutput"),
         dir_prev = ".", which_tests_torun = 1, delete_output = TRUE,
         force_delete_output = TRUE, make_new_ref = FALSE, write_report_to_disk = FALSE)
-      , silent = TRUE)
+      , silent = FALSE)
   )
+  info_res <- paste(names(tp[["res"]]), "=", format(tp[["res"]]), collapse = " / ")
+  info_report <- if (length(tp[["report"]]) > 0) {
+      paste0(c("", rep("* ", length(tp[["report"]]) - 1)), tp[["report"]],
+        collapse = "\n")
+    } else ""
 
   expect_false(inherits(tp, "try-error"))
-  expect_true(all(tp[["res"]][, "has_run"]))
-  expect_false(any(tp[["res"]][, "has_problems"]))
-  expect_null(tp[["report"]])
+  expect_true(all(tp[["res"]][, "has_run"]), info = info_res)
+  expect_false(any(tp[["res"]][, "has_problems"]), info = info_res)
+  expect_null(tp[["report"]], info = info_report)
 })
 
