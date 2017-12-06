3837fd08d986e7c36b5cc2e2a4e3fb41aba6e84c
Zachary Kramer <kramer.zachary.nau@gmail.com>
2017-12-06 15:10:48 +0100

    Close #193: Address feedback about CO2 unit tests

--- tests/testthat/test_CO2.R
+++ tests/testthat/test_CO2.R
@@ -11,16 +11,20 @@ test_that("LookupCarbonScenario: read_csv", {
   ppm_data <- SFSW2_read_csv(LookupCarbonScenarios)
   expect_is(ppm_data, 'data.frame')
   
-  # Check that there are enough rows
-  expect_equal(length(ppm_data$Year), 2498)
-  
-  # Check that the columns are of the correct type
-  columns_with_ppm_data <- 2:ncol(ppm_data)
-  acceptable_data_types <- c('integer', 'double')
+  # Check the columns...
+  # 1) The first column must be "Year"
+  expect_equal(colnames(ppm_data)[1], "Year")
+  # 2) We need at least one scenario
+  expect_gt(ncol(ppm_data), 1)
+  # 3) The years must be integers, because SOILWAT2 expects it as such
   expect_is(ppm_data$Year, 'integer')
-  for (i in columns_with_ppm_data)
+  # 4) The ppm data can be of type integer or double, because the values are
+  # eventually coerced to doubles
+  acceptable_data_types <- c('integer', 'double')
+  for (i in 1:ncol(ppm_data))
   {
-    expect_true(typeof(ppm_data[, i]) %in% acceptable_data_types)
-    expect_false(any(is.na(ppm_data[, i])))  # Check if any NA values exist
+    if (i > 1) expect_true(typeof(ppm_data[, i]) %in% acceptable_data_types)
+    # 5) Check all columns for NA values
+    expect_false(any(is.na(ppm_data[, i])))
   }
 })
