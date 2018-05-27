f1cbc5fa7152ee70f0c63f41e04f623f9c7fed0d
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2018-05-27 15:29:47 -0400

    Passing lint unit tests: `R/Synchronicity.R`

--- R/Synchronicity.R
+++ R/Synchronicity.R
@@ -3,24 +3,25 @@
 
 #' Synchronicity with backing store locks
 #'
-#' @section Note: My attempts at using lock/unlock functions from \pkg{flock} failed
-#'  because unlock appeared to not free resources. My attempts at using such functions
-#'  from \pkg{synchronicity} failed because of segfaults with
-#'  \code{GetResourceName(m@mutexInfoAddr)}; additionally, \pkg{synchronicity} is
-#'  restricted to unix systems. Instead, I use here ideas based on functions
-#'  from \pkg{Rdsm} and Samuel.
+#' @section Note: My attempts at using lock/unlock functions from \pkg{flock}
+#'   failed because unlock appeared to not free resources. My attempts at using
+#'   such functions from \pkg{synchronicity} failed because of segfaults with
+#'   \code{GetResourceName(m@mutexInfoAddr)}; additionally, \pkg{synchronicity}
+#'   is restricted to unix systems. Instead, I use here ideas based on functions
+#'   from \pkg{Rdsm} and Samuel.
 #'
 #' @param lock An object of class \code{SFSW2_lock}.
 #'
 #' @aliases lock unlock lock_init lock_attempt unlock_access lock_access
-#'  check_lock_content remove_lock
+#'   check_lock_content remove_lock
 #' @name synchronicity
 NULL
 
 #' Create and initialize an object of class \code{SFSW2_lock}
 #'
 #' @param fname_lock A character string. Path to locking directory.
-#' @param id A R object. Identifier used to test unique write access in locking directory.
+#' @param id A R object. Identifier used to test unique write access in locking
+#'   directory.
 #' @return An object of class \code{SFSW2_lock}.
 #' @rdname synchronicity
 #' @export
@@ -39,7 +40,8 @@ lock_init <- function(fname_lock, id) {
 
 #' Check content of a backing store lock
 #'
-#' @return A logical value. \code{TRUE} if lock file contains proper code of the lock.
+#' @return A logical value. \code{TRUE} if lock file contains proper code of the
+#'   lock.
 #' @rdname synchronicity
 #' @export
 check_lock_content <- function(lock) {
@@ -53,7 +55,8 @@ check_lock_content <- function(lock) {
 
 #' Remove the files associated with a backing store lock
 #'
-#' @return The return value of deleting the directory associated with \code{lock}.
+#' @return The return value of deleting the directory associated with
+#'   \code{lock}.
 #' @rdname synchronicity
 #' @export
 remove_lock <- function(lock) {
@@ -96,11 +99,11 @@ lock_attempt <- function(lock) {
 #'
 #' Access to the backing store lock is attempted until successfully obtained.
 #'
-#' @param verbose A logical value. If \code{TRUE}, then each attempt at obtaining the lock
-#'  is printed.
+#' @param verbose A logical value. If \code{TRUE}, then each attempt at
+#'   obtaining the lock is printed.
 #' @param seed A seed set, \code{NULL}, or \code{NA}. \code{NA} will not affect
-#'  the state of the RNG; \code{NULL} will re-initialize the RNG; and all other values
-#'  are passed to \code{\link{set.seed}}.
+#'   the state of the RNG; \code{NULL} will re-initialize the RNG; and all other
+#'   values are passed to \code{\link{set.seed}}.
 #'
 #' @return The updated \code{lock} object of class \code{SFSW2_lock}.
 #' @rdname synchronicity
@@ -109,8 +112,10 @@ lock_access <- function(lock, verbose = FALSE, seed = NA) {
   if (!is.na(seed)) set.seed(seed)
 
   if (inherits(lock, "SFSW2_lock")) while (!lock$obtained) {
-    if (verbose)
-      print(paste(Sys.time(), "attempt to obtain lock for", shQuote(lock$locker_id)))
+    if (verbose) {
+      print(paste(Sys.time(), "attempt to obtain lock for",
+        shQuote(lock$locker_id)))
+    }
     lock <- lock_attempt(lock)
     Sys.sleep(stats::runif(1, 0.02, 0.1))
   }
