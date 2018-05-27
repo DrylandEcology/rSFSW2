b2ea53ff03bafb48eaf74b5deb431fbf770b5a7d
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2018-05-27 14:53:02 -0400

    Passing lint unit tests: `R/SoilMTRegimes.R`

--- tests/testthat/test_rSFSW2_StyleSpelllingPractices.R
+++ tests/testthat/test_rSFSW2_StyleSpelllingPractices.R
@@ -37,7 +37,7 @@ test_that("Package code style", {
       "../../R/rSOILWAT2_DataAccess.R",
       "../../R/Simulation_Project.R",
 #      "../../R/Simulation_Run.R",
-#      "../../R/SoilMTRegimes.R",
+      "../../R/SoilMTRegimes.R",
 #      "../../R/Soils_Functions.R",
 #      "../../R/Spatial_Functions.R",
 #      "../../R/Status_Trackers.R",
