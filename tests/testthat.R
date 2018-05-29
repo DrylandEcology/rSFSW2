f2889d30cdb17e2994902139463075699021e600
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2018-05-29 14:42:08 -0400

    Make `skip_on_cran` only skip if NOT_CRAN = "false"
    `testthat::skip_on_cran()` requires a value of "true" for the environmental variable "NOT_CRAN" in order to not skip a test.
    However, "NOT_CRAN" is only set by the package "devtools". For instance, `R CMD check *tar.gz` without `--as-cran` will not set "NOT_CRAN".
    Thus, `testthat::skip_on_cran()` skips even though unintended
    
    --> now, we make sure that "NOT_CRAN" is set to either "true" or "false"

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,15 @@
 library("testthat")
 library("rSFSW2")
 
+# Make sure that environmental variable \code{NOT_CRAN} is set to either
+# \var{\dQuote{true}} or \var{\dQuote{false}}
+if (!identical(tolower(Sys.getenv("NOT_CRAN")), "false")) {
+  # \code{testthat::skip_on_cran()} requires a value of \var{\dQuote{true}}
+  # for the environmental variable \code{NOT_CRAN} to not skip.
+  # However, only \pkg{devtools} sets \code{NOT_CRAN}. For instance,
+  # `R CMD check *tar.gz` without `--as-cran`) does not set \code{NOT_CRAN},
+  # thus, \code{testthat::skip_on_cran()} skips even though unintended
+  Sys.setenv(NOT_CRAN = "true")
+}
+
 test_check("rSFSW2", reporter = ListReporter, encoding = "UTF-8")
