DrylandEcology/rSFSW2

commit 2530215500731c2dba5340603d92af398d884796

Updates for soil functions
- moved some soil functionality from rSOILWAT2 to package rSW2data
- reflecting rSOILWAT2 commit dbe0cc33e12b57282143086b37599b875e1d973e and rSW2data commit f300a0f0081be474f90176b3a8d421ecbf04a73b

R/PriorCalculations.R
@@ -86,15 +86,30 @@ calc_RequestedSoilLayers <- function(SFSW2_prj_meta,
       if (length(req_sd_toadd) == 0) next
 
       # Add identified layers
-      sw_input_soils_data2 <- lapply(seq_along(var_layers),
-        function(iv) sw_input_soils_data[[iv]][il_set, , drop = FALSE])
+      sw_input_soils_data2 <- lapply(
+        seq_along(var_layers),
+        function(iv) sw_input_soils_data[[iv]][il_set, , drop = FALSE]
+      )
+      s
       for (lnew in req_sd_toadd) {
         ilnew <- findInterval(lnew, ldset)
-        il_weight <- calc_weights_from_depths(ilnew, lnew, ldset)
-        sw_input_soils_data2 <- lapply(seq_along(var_layers), function(iv)
-          add_layer_to_soil(sw_input_soils_data2[[iv]], il = ilnew,
-            w = il_weight, method = if (var_layers[iv] %in% sl_vars_sub)
-              "exhaust" else "interpolate"))
+        il_weight <- rSW2data::calc_weights_from_depths(ilnew, lnew, ldset)
+
+        sw_input_soils_data2 <- lapply(
+          seq_along(var_layers),
+          function(iv) {
+            rSW2data::add_layer_to_soil(
+              sw_input_soils_data2[[iv]],
+              il = ilnew,
+              w = il_weight,
+              method = if (var_layers[iv] %in% sl_vars_sub) {
+                "exhaust"
+              } else {
+                "interpolate"
+              }
+            )
+          }
+        )
         ldset <- sort(c(ldset, lnew))
       }
 

R/Simulation_Run.R
@@ -343,7 +343,7 @@ do_OneSite <- function(i_sim, i_SWRunInformation, i_sw_input_soillayers,
     #layer sequence
     ld <- setLayerSequence(soilLayers_N)
     layers_depth <- adjustLayersDepth(layers_depth, soilLayers_N)
-    layers_width <- getLayersWidth(layers_depth)
+    layers_width <- rSW2data::getLayersWidth(layers_depth)
 
     #top and bottom layer aggregation
     DeepestTopLayer <- setDeepestTopLayer(layers_depth, opt_agg[["aon_toplayer_cm"]])
@@ -801,7 +801,7 @@ do_OneSite <- function(i_sim, i_SWRunInformation, i_sw_input_soillayers,
         soilLayers_N <- min(length(layers_depth.datafile), soilLayers_N, na.rm = TRUE)
         soilLayers_N <- max(1, soilLayers_N, na.rm = TRUE)
         layers_depth <- adjustLayersDepth(layers_depth.datafile, soilLayers_N)
-        layers_width <- getLayersWidth(layers_depth)
+        layers_width <- rSW2data::getLayersWidth(layers_depth)
         ld <- setLayerSequence(soilLayers_N)
 
         DeepestTopLayer <- setDeepestTopLayer(layers_depth, opt_agg[["aon_toplayer_cm"]])
@@ -852,7 +852,7 @@ do_OneSite <- function(i_sim, i_SWRunInformation, i_sw_input_soillayers,
         if (soilLayers_N != k) {
           soilLayers_N <- k
           layers_depth <- adjustLayersDepth(layers_depth, soilLayers_N)
-          layers_width <- getLayersWidth(layers_depth)
+          layers_width <- rSW2data::getLayersWidth(layers_depth)
           ld <- setLayerSequence(soilLayers_N)
 
           DeepestTopLayer <- setDeepestTopLayer(layers_depth, opt_agg[["aon_toplayer_cm"]])
@@ -1861,7 +1861,7 @@ do_OneSite <- function(i_sim, i_SWRunInformation, i_sw_input_soillayers,
     #get soil texture data for each layer
     stemp <- rSOILWAT2::swSoils_Layers(swRunScenariosData[[1]])
     layers_depth <- stemp[, 1]
-    layers_width <- getLayersWidth(layers_depth)
+    layers_width <- rSW2data::getLayersWidth(layers_depth)
     soilDepth_cm <- max(stemp[, 1])
     soilLayers_N <- length(stemp[, 1])
     ld <- setLayerSequence(soilLayers_N)

R/Soils_Functions.R
@@ -1,8 +1,3 @@
-# nolint start
-getLayersWidth <- rSOILWAT2:::getLayersWidth
-calc_weights_from_depths <- rSOILWAT2:::calc_weights_from_depths
-add_layer_to_soil <- rSOILWAT2:::add_layer_to_soil
-# nolint end
 
 #' The wrapper only handles 1-cm resolution of soil depths
 #' (mainly because of the \var{trco})

commit 0df9fc5a34fbfa3fa2c5a3eeb21808eba6326e7a

Fix lingering issues from `reorganize_rSW2`
- Fix function `setup_time_simulation_project()`
* it uses function `setup_time_simulation_run()` which was moved from package "rSOILWAT2" (a61532fb58f8174c062f94ec11bc45e5b2078485) to "rSW2data" (ea6d6600d482fd389f492f12c035304d372c48a9)
* this is an oversight by commit e8ad3863c6f99c1f464b85faa2f4c02e8610a5be

- fix function `fields_dailyNRCS_SoilMoistureTemperatureRegimes()`
* it uses several functions which were moved from package "rSOILWAT2" (e93120204f141738ae55e6367363870a02d5c997) to "rSW2funs" (c92d0beed2bb860821db42940089aecff3fe66a2)
* this is an oversight by commit 67e53b536c1929da89b1bc92c92c9905173be263

- fix function `calc_RequestedSoilLayers()`
* copy/edit mistake introduced by commit 2530215500731c2dba5340603d92af398d884796

R/Aggregation_Functions.R
@@ -790,9 +790,9 @@ fields_dailyNRCS_SoilMoistureTemperatureRegimes <- function(aon, ...) {
 
   if (isTRUE(aon[[id]])) {
     temp <- paste0("NRCS_", c(
-      paste0("SoilTemperatureRegime_", rSOILWAT2::STR_names()),
-      paste0("SoilMoistureRegime_", rSOILWAT2::SMR_names()),
-      paste0("SoilMoistureRegimeQualifier_", rSOILWAT2::SMRq_names())))
+      paste0("SoilTemperatureRegime_", rSW2funs::STR_names()),
+      paste0("SoilMoistureRegime_", rSW2funs::SMR_names()),
+      paste0("SoilMoistureRegimeQualifier_", rSW2funs::SMRq_names())))
   }
 
   list(aon = id, N = length(temp), fields = list(coerce_sqlNames(temp)))

R/PriorCalculations.R
@@ -90,7 +90,7 @@ calc_RequestedSoilLayers <- function(SFSW2_prj_meta,
         seq_along(var_layers),
         function(iv) sw_input_soils_data[[iv]][il_set, , drop = FALSE]
       )
-      s
+
       for (lnew in req_sd_toadd) {
         ilnew <- findInterval(lnew, ldset)
         il_weight <- rSW2data::calc_weights_from_depths(ilnew, lnew, ldset)

R/Time_SimulationWorld.R
@@ -72,12 +72,12 @@ get_simulation_time <- function(st, SFSW2_prj_inputs) {
 #'   if \code{add_st2} is \code{TRUE}.
 #' @param A named list, i.e., the updated version of \code{sim_time}.
 #'
-#' @seealso \code{\link[rSOILWAT2]{setup_time_simulation_run}}
+#' @seealso \code{\link[rSW2data]{setup_time_simulation_run}}
 setup_time_simulation_project <- function(sim_time, is_idem = FALSE,
   add_st2 = FALSE, adjust_NS = FALSE,
   use_doy_range = FALSE, doy_ranges = list()) {
 
-  sim_time <- rSOILWAT2::setup_time_simulation_run(sim_time = sim_time)
+  sim_time <- rSW2data::setup_time_simulation_run(sim_time = sim_time)
 
   if (is.matrix(sim_time[["future_yrs"]])) {
     stopifnot(dim(sim_time[["future_yrs"]])[2] == 3)

man/setup_time_simulation_project.Rd
@@ -40,5 +40,5 @@ days indicated in the \code{doy_ranges}.}
 Describe the time of a simulation project
 }
 \seealso{
-\code{\link[rSOILWAT2]{setup_time_simulation_run}}
+\code{\link[rSW2data]{setup_time_simulation_run}}
 }