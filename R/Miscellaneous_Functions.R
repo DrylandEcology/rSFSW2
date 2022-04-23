DrylandEcology/rSFSW2

commit badb781f7d6561d0b9695ded677a714f3b83826a

Multiple time periods for each "idem" daily climate projection
- previously, "idem" daily climate projection could only handle one simulation period (one row of "future_yrs") per scenario-experiment x climate model combination which was called "dall"
- the weather database continues to use "dall"; however, it is capable to subset by start and end year

-> this update introduces multiple time periods for each scenario-experiment x climate model combination
* "future_yrs" requires named list elements to identify scenario-experiments
* the code needs to keep to separate lists of "climate projection" names: "id_to_dbW" to identify data in the weather database; and "id_sim" to identify scenarios (including time periods) during simulations

-> `setup_scenarios()` eliminated the returned elements "id" and "N" and instead provides all information in an expanded data.frame "df" with columns "id_sim" and "id_to_dbW"
-> `setup_time_simulation_project()` now checks if `future_yrs` has named elements; if there are names and if "idem", then multiple time periods for each "idem" daily climate projection are created

R/Miscellaneous_Functions.R
@@ -788,37 +788,67 @@ convert_to_todo_list <- function(x) {
 setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
   #--- Create complete scenario names
   # make sure 'ambient' is not among models
-  temp <- grep(sim_scens[["ambient"]],
+  tmp_simnames <- grep(
+    sim_scens[["ambient"]],
     sim_scens[["models"]],
     invert = TRUE,
     value = TRUE
   )
 
-  if (length(temp) > 0) {
+  if (length(tmp_simnames) > 0) {
     if (is_idem) {
-      # Use all years
-      temp <- paste0("idem.dall.", temp)
+      # Use all years (default)
+      tmp_dbW <- tmp_sim <- paste0("idem.dall.", tmp_simnames)
+
+      # Use multiple time periods
+      # (if `future_yrs` has adequate rownames, i.e., "scenario_YYYYtoYYYY")
+      rns <- rownames(sim_time[["future_yrs"]])
+      tmp_rns <- strsplit(rns, split = "_", fixed = TRUE)
+
+      if (isTRUE(tmp_rns[[1]] == "dall") && all(lengths(tmp_rns[-1]) == 2)) {
+        # match rows of `future_yrs` to requested `scenario.GCM`
+        tmp_fut_sc <- sapply(tmp_rns[-1], `[`, j = 1)
+        tmp_mn_sc <- sapply(
+          strsplit(tmp_simnames, split = ".", fixed = TRUE),
+          `[`,
+          j = 1
+        )
+        stopifnot(setequal(tmp_fut_sc, tmp_mn_sc))
+        tmp0 <- lapply(
+          tmp_mn_sc,
+          function(x) {
+            rns[-1][match(tmp_fut_sc, x, nomatch = 0) > 0]
+          }
+        )
+
+        tmp_sim <- paste0(
+          "idem.",
+          unlist(tmp0), ".",
+          rep(tmp_simnames, times = lengths(tmp0))
+        )
+      }
+
 
     } else {
       # add (multiple) future_yrs, but only if not using full future daily vals
-      temp <- paste0(
+      tmp <- paste0(
         rownames(sim_time[["future_yrs"]]), ".",
-        rep(temp, each = nrow(sim_time[["future_yrs"]]))
+        rep(tmp_simnames, each = nrow(sim_time[["future_yrs"]]))
       )
 
       # add (multiple) downscaling.method
-      temp <- paste0(
+      tmp_dbW <- tmp_sim <- paste0(
         sim_scens[["method_DS"]], ".",
-        rep(temp, each = length(sim_scens[["method_DS"]]))
+        rep(tmp_simnames, each = length(sim_scens[["method_DS"]]))
       )
     }
+
+  } else {
+    tmp_dbW <- tmp_sim <- NULL
   }
 
 
-  # make sure 'ambient' is first entry
-  id <- c(sim_scens[["ambient"]], temp)
-  N <- length(id)
-
+  # start to set time periods
   itime <- data.frame(
     simstartyr = sim_time[["simstartyr"]],
     endyr = sim_time[["endyr"]]
@@ -827,12 +857,12 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
   #--- Create table with scenario name parts for each scenario
   # ConcScen = concentration scenarios, e.g., SRESs, RCPs
   ctmp <- c(
-    "Downscaling", "DeltaStr_yrs", "ConcScen", "Model", "Delta_yrs", "itime"
+    "Downscaling", "DeltaStr_yrs", "ConcScen", "Model",
+    "Delta_yrs", "itime", "id_sim", "id_to_dbW"
   )
 
   climScen <- data.frame(matrix(
-    NA,
-    nrow = N,
+    nrow = 1 + length(tmp_sim),
     ncol = length(ctmp),
     dimnames = list(NULL, ctmp)
   ))
@@ -844,6 +874,8 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
 
   # Fill in information for ambient scenario
   climScen[1, "Model"] <- sim_scens[["ambient"]]
+  climScen[1, "id_sim"] <- sim_scens[["ambient"]]
+  climScen[1, "id_to_dbW"] <- sim_scens[["ambient"]]
   climScen[1, "ConcScen"] <- if ("tag_aCO2_ambient" %in% names(sim_scens)) {
     paste0(sim_scens[["tag_aCO2_ambient"]], collapse = "|")
   } else {
@@ -851,17 +883,20 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
   }
 
 
-  if (N > 1) {
+  if (length(tmp_sim) > 0) {
     # Fill in information about model-scenario combinations
-    temp <- strsplit(id[-1], split = ".", fixed = TRUE)
-    if (!all(lengths(temp) == 4L)) {
+    climScen[-1, "id_sim"] <- tmp_sim
+    climScen[-1, "id_to_dbW"] <- tmp_sim
+
+    tmp <- strsplit(tmp_sim, split = ".", fixed = TRUE)
+    if (!all(lengths(tmp) == 4L)) {
       stop(
         "'climate.conditions' are mal-formed: they must contain ",
         "4 elements that are concatenated by '.'"
       )
     }
 
-    climScen[-1, ctmp[1:4]] <- do.call(rbind, temp)
+    climScen[-1, ctmp[1:4]] <- do.call(rbind, tmp)
 
 
     # set simulation time periods
@@ -873,7 +908,7 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
       tmp_itime <- sim_time[["future_yrs"]][-1, tmp]
       colnames(tmp_itime) <- colnames(itime)
 
-      stopifnot(length(unique(climScen[-1, "ConcScen"])) == NROW(tmp_itime))
+      # stopifnot(length(unique(climScen[-1, "ConcScen"])) == NROW(tmp_itime))
 
       itime <- rbind(itime, unique(tmp_itime))
       rownames(itime) <- NULL
@@ -884,10 +919,30 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
         apply(itime, 1, paste, collapse = "_")
       )
 
-      climScen[-1, "itime"] <- rep(
-        x = ids_itime,
-        times = table(climScen[-1, "ConcScen"])
-      )
+      if (all(climScen[, "DeltaStr_yrs"] == "dall")) {
+        climScen[-1, "itime"] <- rep(
+          x = ids_itime,
+          times = table(climScen[-1, "ConcScen"])
+        )
+
+      } else {
+        # named `future_yrs` and multiple time periods per projection
+        climScen[-1, "itime"] <- rep(
+          x = ids_itime,
+          times = table(climScen[-1, "DeltaStr_yrs"])
+        )
+
+        # `tmp_sim` and `tmp_dbW` are not identical -> correctly set `id_to_dbW`
+        ids <- match(
+          paste0(climScen[-1, "ConcScen"], ".", climScen[-1, "Model"]),
+          sapply(
+            strsplit(tmp_dbW, split = ".", fixed = TRUE),
+            function(x) paste0(x[-(1:2)], collapse = ".")
+          ),
+          nomatch = 0
+        )
+        climScen[-1, "id_to_dbW"] <- tmp_dbW[ids]
+      }
 
     } else {
       #--- Use current/ambient years and a delta for future runs
@@ -902,11 +957,13 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
     #--- List unique sets of requested scenario name parts
     reqMs <- unique(climScen[-1, "Model"])
     reqCSs <- unique(climScen[-1, "ConcScen"])
-    reqCSsPerM <- lapply(reqMs, function(x)
-      unique(climScen[x == climScen[, "Model"], "ConcScen"])
+    reqCSsPerM <- lapply(
+      reqMs,
+      function(x) unique(climScen[x == climScen[, "Model"], "ConcScen"])
     )
-    reqDSsPerM <- lapply(reqMs, function(x)
-      unique(climScen[x == climScen[, "Model"], "Downscaling"])
+    reqDSsPerM <- lapply(
+      reqMs,
+      function(x) unique(climScen[x == climScen[, "Model"], "Downscaling"])
     )
 
   } else {
@@ -914,10 +971,9 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
     reqMs <- reqCSs <- reqCSsPerM <- reqDSsPerM <- NULL
   }
 
-  c(sim_scens,
+  c(
+    sim_scens,
     list(
-      id = id,
-      N = N,
       df = climScen,
       itime = itime,
       is_idem = is_idem,

R/ExtractData_ClimateDownscaling.R
@@ -5282,7 +5282,7 @@ tryToGet_ClimDB <- function(
         compression_type = dbW_compression_type,
         dbW_digits = dbW_digits,
         getYears = getYears,
-        sim_scen_ids = sim_scens[["id"]],
+        sim_scen_ids = unique(sim_scens[["df"]][, "id_to_dbW"]),
         dir_out_tmp = project_paths[["dir_out_temp"]],
         dir_failed = dir_failed,
         fdbWeather = fdbWeather,
@@ -5647,7 +5647,7 @@ is_NEX <- function(climDB_meta) {
 
 #' Calculate historical and future simulation time slices
 #'
-#' @param sim_time A list with elements \code{future_N}, \code{future_yrs},
+#' @param sim_time A list with elements \code{future_yrs},
 #'  \code{DScur_startyr}, and \code{DScur_endyr}.
 #' @param tbox A data.frame or matrix with two rows \code{start} and
 #'  \code{end} and two columns \code{first} and \code{second} describing years
@@ -5666,7 +5666,7 @@ calc_timeSlices <- function(sim_time, tbox) {
     future_N <- 0
     runs <- "dall"
   } else {
-    future_N <- sim_time[["future_N"]]
+    future_N <- length(deltas)
     runs <- c("historical", deltas)
   }
 
@@ -5696,7 +5696,7 @@ calc_timeSlices <- function(sim_time, tbox) {
     }
 
     # future conditions for downscaling
-    for (it in seq_len(sim_time[["future_N"]])) {
+    for (it in seq_len(future_N)) {
       it4 <- 4L * it
       timeSlices[3 + it4, 4] <- max(
         tbox["start", "second"],
@@ -5837,7 +5837,7 @@ calc_assocYears <- function(
     future_N <- 0
     names_assocYears <- "dall"
   } else {
-    future_N <- sim_time[["future_N"]]
+    future_N <- length(deltas)
     names_assocYears <- c(
       "historical",
       paste0(deltas, ".", rep(reqRCPs, each = future_N))
@@ -6344,7 +6344,9 @@ ExtractClimateChangeScenarios <- function(
       SFSW2_prj_inputs[["SWRunInformation"]][tmp_ids, "WeatherFolder"],
     site_ids =
       SFSW2_prj_meta[["sim_size"]][["runIDs_sites_by_dbW"]],
-    scen_labels = SFSW2_prj_meta[["sim_scens"]][["id"]],
+    scen_labels = unique(
+      SFSW2_prj_meta[["sim_scens"]][["df"]][, "id_to_dbW"]
+    ),
     verbose = verbose
   )
 
@@ -6395,7 +6397,11 @@ ExtractClimateWizard <- function(
     )
   }
 
-  if (SFSW2_prj_meta[["sim_scens"]][["N"]] > 1) {
+  id_dbW <- unique(
+    SFSW2_prj_meta[["sim_scens"]][["df"]][-1, "id_to_dbW"]
+  )
+
+  if (length(id_dbW) > 0) {
     if (any("CMIP3_ClimateWizardEnsembles_Global" %in% SFSW2_prj_meta[["sim_scens"]][["sources"]])) {
       # Maurer EP, Adam JC, Wood AW (2009) Climate model based consensus on the hydrologic impacts of climate change to the Rio Lempa basin of Central America. Hydrology and Earth System Sciences, 13, 183-194.
       # accessed via climatewizard.org on July 10, 2012
@@ -6420,7 +6426,11 @@ ExtractClimateWizard <- function(
       recursive = FALSE
     ))
 
-    if (all(SFSW2_prj_meta[["sim_scens"]][["id"]][-1] %in% list.scenarios.external)) {
+    id_dbW <- unique(
+      SFSW2_prj_meta[["sim_scens"]][["df"]][-1, "id_to_dbW"]
+    )
+
+    if (all(id_dbW %in% list.scenarios.external)) {
       # locations of simulation runs
       locations <- sp::SpatialPoints(
         coords = SFSW2_prj_inputs[["SWRunInformation"]][todos, c("X_WGS84", "Y_WGS84")],
@@ -6430,8 +6440,13 @@ ExtractClimateWizard <- function(
       # keep track of successful/unsuccessful climate scenarios
       include_YN_climscen <- rep(FALSE, SFSW2_prj_meta[["sim_size"]][["runsN_main"]])
 
-      for (sc in seq_len(SFSW2_prj_meta[["sim_scens"]][["N"]] - 1)) {
-        dir_ex_dat.sc <- file.path(dir_ex_dat, SFSW2_prj_meta[["sim_scens"]][["id"]][1 + sc])
+      id_dbW <- unique(
+        SFSW2_prj_meta[["sim_scens"]][["df"]][-1, "id_to_dbW"]
+      )
+
+      for (sc in seq_along(id_dbW)) {
+        dir_ex_dat.sc <- file.path(dir_ex_dat, id_dbW[sc])
+
         tmp <- basename(list.dirs2(
           path = dir_ex_dat.sc,
           full.names = FALSE,
@@ -6542,7 +6557,10 @@ ExtractClimateWizard <- function(
       )
       unlink(SFSW2_prj_meta[["fnames_in"]][["fpreprocin"]])
 
-      include_YN_climscen <- as.numeric(include_YN_climscen >= (SFSW2_prj_meta[["sim_scens"]][["N"]] - 1))
+      tmpn <- length(unique(
+        SFSW2_prj_meta[["sim_scens"]][["df"]][-1, "id_to_dbW"]
+      ))
+      include_YN_climscen <- as.numeric(include_YN_climscen >= tmpn)
       SFSW2_prj_inputs[["SWRunInformation"]][, "Include_YN_ClimateScenarioSources"] <- include_YN_climscen
       utils::write.csv(
         SFSW2_prj_inputs[["SWRunInformation"]],
@@ -6715,7 +6733,9 @@ PrepareClimateScenarios <- function(
           SFSW2_prj_inputs[["SWRunInformation"]][tmp_ids, "WeatherFolder"],
         site_ids =
           SFSW2_prj_meta[["sim_size"]][["runIDs_sites_by_dbW"]][tmp_ids2],
-        scen_labels = SFSW2_prj_meta[["sim_scens"]][["id"]],
+        scen_labels = unique(
+          SFSW2_prj_meta[["sim_scens"]][["df"]][, "id_to_dbW"]
+        ),
         verbose = opt_verbosity[["verbose"]]
       )
 

R/Indices.R
@@ -5,7 +5,7 @@
 #'   loop2b
 #' @param sc An integer value. The index along loop3 \code{1...scN}.
 #' @param scN An integer value. The number of (climate) scenarios used in the
-#'   project, i.e., \eqn{scN == sim_scens[["N"]]}.
+#'   project, i.e., \eqn{scN == nrow(sim_scens[["df"]])}.
 #' @param runN An integer value. The number of runs/sites set up in the main
 #'   input file, i.e., \eqn{runN == runsN_main}.
 #' @param iexp An integer value. The index value along loop1 \code{1...expN}.
@@ -183,7 +183,7 @@ determine_simulation_size <- function(SWRunInformation, include_YN,
   runIDs_total <- seq_len(runsN_total)
   digitsN_total <- 1 + ceiling(log10(runsN_total))  # max index digits
   runsN_job <- runsN_sites * max(expN, 1L)
-  runsN_Pid <- runsN_total * sim_scens[["N"]]
+  runsN_Pid <- runsN_total * nrow(sim_scens[["df"]])
 
   list(expN = expN, runsN_main = runsN_main, runIDs_sites = runIDs_sites,
     runsN_sites = runsN_sites, runsN_total = runsN_total,

R/OutputDatabase.R
@@ -168,12 +168,20 @@ getSiteIds <- function(con, folderNames) {
 #' Get name of weather file from database \var{\sQuote{dbOutput}}
 #' @export
 local_weatherDirName <- function(i_sim, runN, scN, dbOutput) {
-  con <- dbConnect(SQLite(), dbname = dbOutput,
-    flags = SQLITE_RO)
+  con <- dbConnect(
+    SQLite(),
+    dbname = dbOutput,
+    flags = SQLITE_RO
+  )
   on.exit(dbDisconnect(con), add = TRUE)
 
-  dbGetQuery(con, paste("SELECT WeatherFolder FROM header WHERE P_id=",
-    it_Pid(i_sim, runN, 1, scN)))[1, 1]
+  dbGetQuery(
+    con,
+    paste(
+      "SELECT WeatherFolder FROM header WHERE P_id=",
+      it_Pid(i_sim, runN, 1, scN)
+    )
+  )[1, 1]
 }
 
 
@@ -2657,7 +2665,10 @@ dbOutput_create_Design <- function(
 
   sql <- "INSERT INTO scenario_labels VALUES(NULL, :label)"
   rs <- dbSendStatement(con_dbOut, sql)
-  dbBind(rs, params = list(label = SFSW2_prj_meta[["sim_scens"]][["id"]]))
+  dbBind(
+    rs,
+    params = list(label = SFSW2_prj_meta[["sim_scens"]][["df"]][, "id_sim"])
+  )
   dbClearResult(rs)
 
 
@@ -2733,17 +2744,17 @@ dbOutput_create_Design <- function(
   db_runs$P_id <- seq_len(SFSW2_prj_meta[["sim_size"]][["runsN_Pid"]])
   db_runs$label_id <- rep(
     seq_len(SFSW2_prj_meta[["sim_size"]][["runsN_total"]]),
-    each = SFSW2_prj_meta[["sim_scens"]][["N"]]
+    each = nrow(SFSW2_prj_meta[["sim_scens"]][["df"]])
   )
   db_runs$site_id <- rep(
     rep(
       SFSW2_prj_inputs[["SWRunInformation"]]$site_id,
       times = max(SFSW2_prj_meta[["sim_size"]][["expN"]], 1L)
     ),
-    each = SFSW2_prj_meta[["sim_scens"]][["N"]]
+    each = nrow(SFSW2_prj_meta[["sim_scens"]][["df"]])
   )
   db_runs$scenario_id <- rep(
-    seq_len(SFSW2_prj_meta[["sim_scens"]][["N"]]),
+    seq_len(nrow(SFSW2_prj_meta[["sim_scens"]][["df"]])),
     times = SFSW2_prj_meta[["sim_size"]][["runsN_total"]]
   )
 
@@ -2760,14 +2771,17 @@ dbOutput_create_Design <- function(
     if (useExperimentals) {
       rep(
         temp + treatments_unique_map - 1,
-        each = SFSW2_prj_meta[["sim_scens"]][["N"]]
+        each = nrow(SFSW2_prj_meta[["sim_scens"]][["df"]])
       )
     } else {
-      rep(treatments_unique_map, each = SFSW2_prj_meta[["sim_scens"]][["N"]])
+      rep(
+        treatments_unique_map,
+        each = nrow(SFSW2_prj_meta[["sim_scens"]][["df"]])
+      )
     }
   } else {
     if (useExperimentals) {
-      rep(temp, each = SFSW2_prj_meta[["sim_scens"]][["N"]])
+      rep(temp, each = nrow(SFSW2_prj_meta[["sim_scens"]][["df"]]))
     } else 1
   }
 

R/OutputDatabase_Ensembles.R
@@ -4,16 +4,20 @@ update_scenarios_with_ensembles <- function(SFSW2_prj_meta) {
   if (length(sim_scens[["ensemble.levels"]]) > 0)
     sim_scens[["ensemble.levels"]] <- sort(sim_scens[["ensemble.levels"]])
 
-  sim_scens[["has_ensembles"]] <- sim_scens[["N"]] > 1 &&
+  sim_scens[["has_ensembles"]] <- nrow(sim_scens[["df"]]) > 1 &&
     !is.null(sim_scens[["ensemble.families"]]) &&
     length(sim_scens[["ensemble.levels"]]) > 0 &&
     all(is.numeric(sim_scens[["ensemble.levels"]]))
 
   if (sim_scens[["has_ensembles"]]) {
     sim_scens[["ensemble.families"]] <- paste0(rownames(SFSW2_prj_meta[["sim_time"]][["future_yrs"]]), ".",
-      rep(sim_scens[["ensemble.families"]], each = SFSW2_prj_meta[["sim_time"]][["future_N"]]))  #add (multiple) future_yrs
-    scenarios.ineach.ensemble <- sapply(sim_scens[["ensemble.families"]], function(x)
-      grepl(x, sim_scens[["id"]], ignore.case = TRUE))
+      rep(sim_scens[["ensemble.families"]], each = nrow(SFSW2_prj_meta[["sim_time"]][["future_yrs"]])))  #add (multiple) future_yrs
+    scenarios.ineach.ensemble <- sapply(
+      sim_scens[["ensemble.families"]],
+      function(x) {
+        grepl(x, sim_scens[["df"]][, "id_sim"], ignore.case = TRUE)
+      }
+    )
     temp <- apply(scenarios.ineach.ensemble, 2, any)
 
     sim_scens[["ensemble.families"]] <- sim_scens[["ensemble.families"]][temp]
@@ -140,7 +144,7 @@ update_scenarios_with_ensembles <- function(SFSW2_prj_meta) {
       #Grab x rows at a time
       SQL <- paste0("SELECT MAX(P_id) FROM '", Table, "';")
       maxP_id <- as.integer(dbGetQuery(con, SQL))
-      maxRun_id <- (maxP_id/sim_scens[["N"]])
+      maxRun_id <- (maxP_id/nrow(sim_scens[["df"]]))
 
       for (j in 1:length(ensemble.families)) {
         EnsembleTimeStop <- Sys.time() - t.overall
@@ -156,17 +160,17 @@ update_scenarios_with_ensembles <- function(SFSW2_prj_meta) {
         LastPid <- integer(length = length(EnsembleFamilyLevelTables))
         for (i in 1:length(LastPid)) {
           SQL <- paste0("SELECT MAX(P_id) FROM '", EnsembleFamilyLevelTables[i], "';")
-          LastPid[i] <- as.integer(dbGetQuery(conEnsembleDB, SQL))+(sim_scens[["N"]]-1)#Need to add all the scenarios because last P_id will always be Current
+          LastPid[i] <- as.integer(dbGetQuery(conEnsembleDB, SQL))+(nrow(sim_scens[["df"]])-1)#Need to add all the scenarios because last P_id will always be Current
         }
         if (any(is.na(LastPid))) { #If any of the tables are empty we need to start at the beginning
           minRun_id <- 1
         } else {
-          minRun_id <- (min(LastPid)/sim_scens[["N"]])+1 #This is already done so we add one
+          minRun_id <- (min(LastPid)/nrow(sim_scens[["df"]]))+1 #This is already done so we add one
         }
         #########################
         for (i in seq(minRun_id, maxRun_id, opt_chunks[["ensembleCollectSize"]])) {
-          start <- (i-1)*sim_scens[["N"]]+1
-          stop <- (min(i+opt_chunks[["ensembleCollectSize"]]-1, maxRun_id)-1)*sim_scens[["N"]]+sim_scens[["N"]]
+          start <- (i-1)*nrow(sim_scens[["df"]])+1
+          stop <- (min(i+opt_chunks[["ensembleCollectSize"]]-1, maxRun_id)-1)*nrow(sim_scens[["df"]])+nrow(sim_scens[["df"]])
           dataScen.Mean <- read.scenarios(Table = Table, start = start, stop = stop, ensemble.family = ensemble.family, export.header = TRUE)
           Table <- sub(pattern = "Mean", replacement = "stats::sd", Table)
           dataScen_sd <- read.scenarios(Table = Table, start = start, stop = stop, ensemble.family = ensemble.family, export.header = FALSE)

R/Simulation_Project.R
@@ -516,6 +516,21 @@ is_project_description_outdated <- function(meta) {
     meta[["project_paths"]][["dir_log"]] <- meta[["project_paths"]][["dir_prj"]]
   }
 
+  has <- c("id_sim", "id_to_dbW") %in% colnames(meta[["sim_scens"]][["df"]])
+  if (any(!has)) {
+    warning(
+      "Outdated project description: ",
+      "The columns `id_sim` and/or `id_to_dbW` of `sim_scens[['df']]` ",
+      "were not calculated; ",
+      "it was added after v4.3.0; ",
+      "assuming previous behavior, ",
+      "i.e., setting `id_sim` and `id_to_dbW` equal to the previous `id`."
+    )
+
+    meta[["sim_scens"]][["df"]][, "id_to_dbW"] <- meta[["sim_scens"]][["id"]]
+    meta[["sim_scens"]][["df"]][, "id_sim"] <- meta[["sim_scens"]][["id"]]
+  }
+
   meta
 }
 
@@ -1420,7 +1435,9 @@ check_rSFSW2_project_input_data <- function(SFSW2_prj_meta, SFSW2_prj_inputs,
         SFSW2_prj_inputs[["SWRunInformation"]][tmp_ids, "WeatherFolder"],
       site_ids =
         SFSW2_prj_meta[["sim_size"]][["runIDs_sites_by_dbW"]],
-      scen_labels = SFSW2_prj_meta[["sim_scens"]][["id"]],
+      scen_labels = unique(
+        SFSW2_prj_meta[["sim_scens"]][["df"]][, "id_to_dbW"]
+      ),
       verbose = opt_verbosity[["verbose"]]
     )
 
@@ -1792,10 +1809,18 @@ simulate_SOILWAT2_experiment <- function(SFSW2_prj_meta, SFSW2_prj_inputs,
   #------------------------OVERALL TIMING
   delta.overall <- difftime(Sys.time(), t1, units = "secs")
 
-  compile_overall_timer(SFSW2_prj_meta[["fnames_out"]][["timerfile"]],
+  compile_overall_timer(
+    SFSW2_prj_meta[["fnames_out"]][["timerfile"]],
     SFSW2_prj_meta[["project_paths"]][["dir_out"]],
-    SFSW2_glovars[["p_workersN"]], runs.completed,
-    SFSW2_prj_meta[["sim_scens"]][["N"]], 0, delta.overall, NA, 0, 0)
+    SFSW2_glovars[["p_workersN"]],
+    runs.completed,
+    nrow(SFSW2_prj_meta[["sim_scens"]][["df"]]),
+    0,
+    delta.overall,
+    NA,
+    0,
+    0
+  )
 
   if (opt_verbosity[["verbose"]]) {
     print(utils::sessionInfo())

commit 36cfdf7fa287340e1a1eb6d1f2a0e6c857debfc9

Fix typo in `setup_scenarios()`
- typo introduced by commit badb781f7d6561d0b9695ded677a714f3b83826a on April 13, 2022

R/Miscellaneous_Functions.R
@@ -839,7 +839,7 @@ setup_scenarios <- function(sim_scens, sim_time, is_idem = FALSE) {
       # add (multiple) downscaling.method
       tmp_dbW <- tmp_sim <- paste0(
         sim_scens[["method_DS"]], ".",
-        rep(tmp_simnames, each = length(sim_scens[["method_DS"]]))
+        rep(tmp, each = length(sim_scens[["method_DS"]]))
       )
     }
 